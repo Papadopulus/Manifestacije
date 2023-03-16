8bf5d6659d7f8ca1abb9a922c0aed540eb8d05dd
Papadopulus <bogdan1petkovic@gmail.com>
2023-03-16 18:18:25 +0100

    Added nav bar

--- src/client/manifestacije/src/App.js
+++ src/client/manifestacije/src/App.js
@@ -1,12 +1,20 @@
 import LoginInput from "./components/Login/LoginInput";
-import {Fragment} from "react";
+import { Fragment } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-    return (
-        <Fragment>
-            <LoginInput/>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          {/*<Route path="/" element={<Home />} />  <-- treba se dodati home page*/}
+          <Route path="/login" element={<LoginInput />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  );
 }
 
 export default App;
