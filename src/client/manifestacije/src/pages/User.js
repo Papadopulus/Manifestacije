491c5e434f989549a494221613246a5c4d6bc4f7
hottered <aleksa.mitrovic2001@gmail.com>
2023-03-19 10:35:16 +0100

    Gotov Login,ostalo jos role based login

--- src/client/manifestacije/src/pages/User.js
+++ src/client/manifestacije/src/pages/User.js
@@ -1,4 +1,4 @@
-﻿import {useContext, useEffect, useState} from "react";
+﻿import {useContext, useEffect, useRef, useState} from "react";
 // import axios from "axios";
 import axios from "../api/axios";
 import AuthContext from "../store/AuthContext";
@@ -13,17 +13,29 @@ const User = () => {
     
     const [userData,setUserData] = useState([]);
     const {user} = useContext(AuthContext);
+
+    const shouldLog = useRef(true);
     useEffect(  () => {
         
-         jwtInterceptor.get(`https://localhost:7237/users/${user.Id}`)
-            .then((response) => {
-            setUserData(response.data);
-            console.log(response.data);
-        }).catch(error => {
-            console.error(error);
-        })
-        
-        
+        if (shouldLog.current) {
+            // console.log('Mounting...')
+            shouldLog.current =false;
+            const getUserInfo = async () => {
+                await jwtInterceptor.get(`https://localhost:7237/users/${user.Id}`)
+                    .then((response) => {
+                        setUserData(response.data);
+                        console.log(response.data);
+                    }).catch(error => {
+                    console.error(error);
+                })
+            }
+            getUserInfo();
+        }
+        return () => {
+            // console.log('Unmounting...')
+            shouldLog.current = false;
+        }
+
     },[])
     // console.log(userData.firstName)
     return <div>
