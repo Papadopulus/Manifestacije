c3e4568c219ea407f1b381576a06cbb855dd8a88
DarkVader <radepantelej@gmail.com>
2023-03-23 12:55:36 +0100

    .

--- src/client/manifestacije/src/pages/User.js
+++ src/client/manifestacije/src/pages/User.js
@@ -1,21 +1,13 @@
 ﻿import {useContext, useEffect, useRef, useState} from "react";
 import axios from "axios";
-// import axios from "../api/axios";
 import AuthContext from "../store/AuthContext";
-import jwtInterceptor from "../api/interceptors";
-import {request} from "axios";
-import {Await} from "react-router-dom";
 
-
-const User = () => {
-
-    // console.log('User component beeing redner...')
-
+const User = async () => {
     const [userData, setUserData] = useState([]);
     const {user} = useContext(AuthContext);
 
     const shouldLog = useRef(true);
-    useEffect( () => {
+    useEffect(() => {
 
         if (shouldLog.current) {
 
@@ -33,41 +25,32 @@ const User = () => {
                     token: `${tokenData.refreshToken}`
                 };
                 const refresh = async () => {
-                    await axios.post(
-                        "https://localhost:7237/authenticate/refresh",
-                        payload
-                    )
-                        .then(response => {
-                            console.log(response);
-                            localStorage.setItem("tokens", JSON.stringify(response.data))
-                            tokenData = response.data;
-                            console.log("tokenData posle ");
-                            console.log(tokenData)
-                        }).catch(error => {
-                            console.log(error)
-                        })
-
+                    const result = await axios.post("https://localhost:7237/authenticate/refresh", payload)
+                        // .then(response => {
+                        //     console.log(response);
+                        //     localStorage.setItem("tokens", JSON.stringify(response.data))
+                        //     tokenData = response.data;
+                        //     console.log("tokenData posle ");
+                        //     console.log(tokenData)
+                        // }).catch(error => {
+                        //     console.log(error)
+                        // })
+                    localStorage.setItem("tokens", JSON.stringify(result.data))
+                    return result.data
                 }
                 refresh();
             }
             const getUsersData = async () => {
-                setTimeout(() => {
-
-                },1000)
-                console.log("token koji se salje ");
-                const tokenKojiSeSalje = localStorage.getItem("tokens");
-                console.log(tokenKojiSeSalje);
-                let header = {
-                    "Authorization": `Bearer ${tokenData.token}`
-                }
-                await axios.get(`https://localhost:7237/users/${user.Id}`, {headers: header})
-                    .then((response) => {
-                        setUserData(response.data);
-                        console.log("podaci")
-                        console.log(response.data);
-                    }).catch(error => {
-                        console.error(error);
-                    })
+                setTimeout(async () => {
+                    console.log("token koji se salje ");
+                    const tokenKojiSeSalje = localStorage.getItem("tokens");
+                    console.log(tokenKojiSeSalje);
+                    let header = {
+                        "Authorization": `Bearer ${tokenData.token}`
+                    }
+                    const result = await axios.get(`https://localhost:7237/users/${user.Id}`, {headers: header})
+                    return result.data
+                }, 1000)
             }
             getUsersData()
         }
