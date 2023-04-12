73291fe8193201155ee92db35523b4e2e39015a1
rakodalovic <rako.odalovic@hotmail.rs>
2023-04-12 17:17:44 +0200

    typewriter animation

--- src/client/manifestacije/src/components/Introduction/Introduction.js
+++ src/client/manifestacije/src/components/Introduction/Introduction.js
@@ -1,26 +1,25 @@
 import './Introduction.css'
 import React from "react";
-import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import Typewriter from 'typewriter-effect';
 const Introduction=()=>{
-    const {text}=useTypewriter({
-        words:['Festival','Koncert','Žurku'],
-        loop:{},
-        typeSpeed: 120
-    })
+    const words = ['Koncert?', 'Festival?', 'Žurku?'];
     return(
         <div className={"background-div"}>
             <div className={"main-div"}>
                 <div className={"header-text"}>
                     manifestacije
-                    <span>{text}</span>
-                    <Cursor></Cursor>
                 </div>
                 <div className={"body-text"}>
-                    Dobrodošli na najuzbudljiviji portal o dešavanjima u Srbiji!
-                    Ovde ćete pronaći sve što vam je potrebno za uživanje u najboljim događajima u zemlji.
-                    Bilo da ste ljubitelj muzike, hrane, sporta ili kulture, imamo za svakoga ponešto.
-                    Pripremite se za zabavu, upoznavanje novih ljudi i stvaranje uspomena koje će trajati ceo život.
-                    Dobrodošli u svet manifestacija u Srbiji!
+                    <span className={"space"}>Hoćemo na</span>
+                    <Typewriter
+                        options={{
+                            strings: words,
+                            autoStart: true,
+                            loop: true,
+                            deleteSpeed: 50,
+                            pauseFor: 1000,
+                        }}
+                    />
                 </div>
             </div>
         </div>
