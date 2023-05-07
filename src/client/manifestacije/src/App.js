983854a20a9c2701dd249efbcaf8c7132fc72e4c
hottered <aleksa.mitrovic2001@gmail.com>
2023-05-07 18:35:24 +0200

    userListsAdmin v4

--- src/client/manifestacije/src/App.js
+++ src/client/manifestacije/src/App.js
@@ -45,6 +45,7 @@ function App() {
                         <Route path="/reset" element={<PasswordReset />} />
 
                     </Routes>
+                    
                 </AuthContextProvider>
             </BrowserRouter>
 
