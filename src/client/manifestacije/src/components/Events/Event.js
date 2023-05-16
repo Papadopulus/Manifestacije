39e800ade57b3e524113b5cb1f413d4c29420296
Papadopulus <bogdan1petkovic@gmail.com>
2023-05-16 20:55:26 +0200

    Rale mora da se pozabavi css-om jer sam paraplegicar

--- src/client/manifestacije/src/components/Events/Event.js
+++ src/client/manifestacije/src/components/Events/Event.js
@@ -2,26 +2,39 @@
 import classes from "./Event.module.css";
 import { format } from "date-fns";
 function Event({ event }) {
+  function onClickEventHandler() {
+    console.log("Jebem");
+  }
+
   return (
     <div className={classes["listItem-wrap"]}>
-      <img src={event.imageUrls} alt="" />
+      <img src={event.imageUrls} alt="" onClick={onClickEventHandler} />
       <header>
         <h4>{event.title}</h4>
-        {/*<span>🌟{rating}</span>*/}
-        <p>{event.description}</p>
       </header>
+
       <footer>
+        <b>
+          <i className="fa fa-calendar" aria-hidden="true"></i>
+          {format(new Date(event.startingDate), "dd-MMMM")}-
+          {format(new Date(event.endingDate), "dd-MMMM")} (
+          {format(new Date(event.endingDate), "yyyy")})
+        </b>
+
         <p>
           <b>
-            {format(new Date(event.startingDate), "dd-MMMM")}-
-            {format(new Date(event.endingDate), "dd-MMMM")} (
-            {format(new Date(event.endingDate), "yyyy")})
+            <i className="fa fa-ticket" aria-hidden="true"></i> $
+            {event.ticketPrice}
+            rsd
           </b>
         </p>
-        <p>
-          <b>${event.ticketPrice}rsd</b>
-        </p>
       </footer>
+      <div className={classes["listItem-description"]}>
+        <p>
+          <b>Opis:</b>
+        </p>
+        <p>{event.description}</p>
+      </div>
     </div>
   );
 }
