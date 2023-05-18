7711ef35acac7d36b013edeb3f7986f58ffcd065
DarkVader <radepantelej@gmail.com>
2023-05-18 13:04:31 +0200

    .

--- src/server/Manifestacije.ImageService/Program.cs
+++ src/server/Manifestacije.ImageService/Program.cs
@@ -7,6 +7,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policyBuilder =>
+    {
+        policyBuilder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowAll");
 app.UseStaticFiles();
 
 app.UseHttpsRedirection();
